391ff9cea09996ca6b42bee8029b93bc0a4c05a2
Jon Moore <jonrmoore@protonmail.com>
2018-09-30 23:28:50 -0500

    Titles, authors, and publishers assigned to separate keys

--- server/routes/nebula.js
+++ server/routes/nebula.js
@@ -1,20 +1,22 @@
-
-// "https://nebulas.sfwa.org/search-awards/?sa=1&award[7]=on&statusfilter=both&start_year=1975&end_year=1975&search="
 const express = require('express');
 const router = express.Router();
 
 const scrapeIt = require('scrape-it');
 
-function extractBookInfo(text) {
-  const re = /(^.*), by (.*) \((.*)\)$/g
-  const match = re.exec(text);
-  return { title: match[1]};
-}
+// TODO
+// Create method to check for lowercase letters followed by capitals and insert a
+// comma followed by a whitespace.
 
 function getBestBooks(year) {
   return scrapeIt(`https://nebulas.sfwa.org/search-awards/?sa=1&award[7]=on&statusfilter=both&start_year=${year}&end_year=${year}&search=`, {
-    title: ".results ul.award_list li > a > em"
-  });
+    titles: ".results ul.award_list li > a:nth-of-type(1)",
+    authors: ".results ul.award_list li > a:nth-of-type(2)",
+    publishers: ".results ul.award_list li > a:nth-of-type(3)"
+      
+    // TODO
+    // =====
+    // Use convert() method and regex to divide results 
+  })   
 }
 
 router.get("/nebula", (req, res, next) => {
