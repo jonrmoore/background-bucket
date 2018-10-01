f815b64b0ee849e190cca494edcd8f05427dffd3
Jon Moore <jonrmoore@protonmail.com>
2018-09-30 20:33:01 -0500

    Nebula now yields data, which is good.

--- server/routes/nebula.js
+++ server/routes/nebula.js
@@ -5,23 +5,20 @@ const router = express.Router();
 
 const scrapeIt = require('scrape-it');
 
-// function extractBookInfo(text) {
-//   const re = /(^.*), by (.*) \((.*)\)$/g
-//   const match = re.exec(text);
-//   return { title: match[1]};
-// }
+function extractBookInfo(text) {
+  const re = /(^.*), by (.*) \((.*)\)$/g
+  const match = re.exec(text);
+  return { title: match[1]};
+}
 
 function getBestBooks(year) {
   return scrapeIt(`https://nebulas.sfwa.org/search-awards/?sa=1&award[7]=on&statusfilter=both&start_year=${year}&end_year=${year}&search=`, {
-    winner: {
-      selector: '.results ul.award_list li a',
-      attr: "href"
-    }
+    title: ".results ul.award_list li > a > em"
   });
 }
 
 router.get("/nebula", (req, res, next) => {
-  getBestBooks(2018).then(({ data }) => {
+  getBestBooks(1975).then(({ data }) => {
     res.json(data)
   }).catch(console.error);
 })
